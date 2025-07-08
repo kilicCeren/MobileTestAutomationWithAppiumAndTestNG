579b9080c434344dd142e4183a825e5e76772106
kilicCeren <kiliccrn07@gmail.com>
2025-07-08 17:45:43 +0300

    v1

--- src/test/java/tests/day01/HesapMak.java
+++ src/test/java/tests/day01/HesapMak.java
@@ -21,32 +21,30 @@ public class HesapMak {
     // android icin ayri ios icin ayri ozellikleri bulunan driverlar uretti. IOSDriver<IOSElement> iosDriver;
 
 
-
-            // kullanici gerekli kurulumlari yapar
-            // uygulamanin yuklendigini dogrular(isInstalled)
-            // uygulamanin acildigini dogrular
-            // 400 un 3 katininin 1200 oldugunu hesap makinasindan dogrulayalim
-
+    // kullanici gerekli kurulumlari yapar
+    // uygulamanin yuklendigini dogrular(isInstalled)
+    // uygulamanin acildigini dogrular
+    // 400 un 3 katininin 1200 oldugunu hesap makinasindan dogrulayalim
 
 
     @Test
-    public void hesapMak()throws MalformedURLException {
+    public void hesapMak() throws MalformedURLException {
         // kullanici gerekli kurulumlari yapar
-        DesiredCapabilities caps =new DesiredCapabilities();
+        DesiredCapabilities caps = new DesiredCapabilities();
         //caps.setCapability("deviceName","Pixel 148");
-        caps.setCapability(MobileCapabilityType.DEVICE_NAME,"Pixel 148");
-        caps.setCapability(MobileCapabilityType.PLATFORM_NAME,"Android");
-        caps.setCapability(MobileCapabilityType.PLATFORM_VERSION,"10.0");
-        caps.setCapability(MobileCapabilityType.AUTOMATION_NAME,"UiAutomator2");
-        caps.setCapability(MobileCapabilityType.APP,"C:\\Users\\Ceren\\IdeaProjects\\MobileTestAutomationWithAppiumAndTestNG\\Apps\\Calculator_8.4 (503542421)_Apkpure (3).apk");
+        caps.setCapability(MobileCapabilityType.DEVICE_NAME, "Pixel 148");
+        caps.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
+        caps.setCapability(MobileCapabilityType.PLATFORM_VERSION, "10.0");
+        caps.setCapability(MobileCapabilityType.AUTOMATION_NAME, "UiAutomator2");
+        caps.setCapability(MobileCapabilityType.APP, "C:\\Users\\Ceren\\IdeaProjects\\MobileTestAutomationWithAppiumAndTestNG\\Apps\\Calculator_8.4 (503542421)_Apkpure (3).apk");
 
 
-    //App capability bir uygulama eger yuklu degilse uygulamayi cihaza yuklemek icin kullanilir
-    //Eger uygulama yukluyse ve tekrardan test calistirilirsa App capability uygulama yuklu mu diye kontrol eder
-    //eger yuklu degilse uygulamayi yukler, eger yukluyse uygulamayi acar
+        //App capability bir uygulama eger yuklu degilse uygulamayi cihaza yuklemek icin kullanilir
+        //Eger uygulama yukluyse ve tekrardan test calistirilirsa App capability uygulama yuklu mu diye kontrol eder
+        //eger yuklu degilse uygulamayi yukler, eger yukluyse uygulamayi acar
 
 
-        driver =new AndroidDriver<AndroidElement>(new URL("http://127.0.0.1:4723/wd/hub"),caps);
+        driver = new AndroidDriver<AndroidElement>(new URL("http://127.0.0.1:4723/wd/hub"), caps);
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 
         // uygulamanin yuklendigini dogrular(isInstalled)
@@ -62,10 +60,10 @@ public class HesapMak {
         driver.findElementByAccessibilityId("multiply").click();
         driver.findElementByAccessibilityId("3").click();
 
-        String islemSonucu=driver.findElementById("com.google.android.calculator:id/result_preview").getText();
-        int bekledigimSonuc=1200;
+        String islemSonucu = driver.findElementById("com.google.android.calculator:id/result_preview").getText();
+        int bekledigimSonuc = 1200;
 
-        Assert.assertEquals(Integer.parseInt(islemSonucu),bekledigimSonuc);
+        Assert.assertEquals(Integer.parseInt(islemSonucu), bekledigimSonuc);
 
 
     }
